896ba00a8edd0421589c5289fa4b086f7f72081a
Kendolay venkatesh <kvenkatesh462@gmail.come>
2022-09-08 14:44:28 +0000

    Import and Export an Object using Common JS Module Syntax

---
+++ exportObject.js
@@ -0,0 +1,8 @@
+class PersonDetailes {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+module.exports = PersonDetailes;
---
+++ importObject.js
@@ -0,0 +1,4 @@
+const PersonDetailes = require("./exportObject");
+const personDetailes = new PersonDetailes("John", "Wilson");
+console.log(personDetailes.firstName);
+console.log(personDetailes.lastName);
